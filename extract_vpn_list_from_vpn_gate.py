0d89bf182eb262e42b6cf97ddfe90e8d8116a729
lwt <weitaoli.echung@gmail.com>
2015-04-30 11:14:45 +0800

    getting the open vpn url list from a local html of vpn gate getting the download urls from each vpn url

--- extract_vpn_list_from_vpn_gate.py
+++ extract_vpn_list_from_vpn_gate.py
@@ -2,6 +2,7 @@
 __author__ = 'VDTConstructor'
 import re
 import logging
+# import urllib
 
 from bs4 import BeautifulSoup
 
@@ -28,7 +29,7 @@ def get_open_vpn_configure_url(vpn_gate_htm_file):
 				logging.debug('the td length is %d', len(td_list))
 				if not title_initialed:
 					for td in td_list:
-						title[td.get_text()] = len(title)
+						title[td.get_text().lower()] = len(title)
 					title_initialed = True
 				else:
 					vpn = []
@@ -38,22 +39,22 @@ def get_open_vpn_configure_url(vpn_gate_htm_file):
 					all_vpn_list.append(vpn)
 	for t in title:
 		logging.info('%s', t)
-	logging.info('%d', title[u'''国家 / 地区(物理位置)'''])
-	logging.info('%d', title[u'''DDNS 主机名IP 地址(ISP 主机名)'''])
-	logging.info('%d', title[u'''VPN 会话数运行时间累计用户数'''])
-	logging.info('%d', title[u'''线路质量吞吐量和 Ping累积转移日志策略'''])
-	logging.info('%d', title[u'''SSL-VPNWindows(合适的)'''])
-	logging.info('%d', title[u'''L2TP/IPsecWindows, Mac,iPhone, Android无需 VPN 客户端'''])
-	logging.info('%d', title[u'''OpenVPNWindows, Mac,iPhone, Android'''])
-	logging.info('%d', title[u'''MS-SSTPWindows Vista,7, 8, RT无需 VPN 客户端'''])
-	logging.info('%d', title[u'''志愿者操作员的名字(+ 操作员的消息)'''])
-	logging.info('%d', title[u'''总分(质量)'''])
+	# logging.info('%d', title[u'''国家 / 地区(物理位置)'''])
+	# logging.info('%d', title[u'''ddns 主机名ip 地址(isp 主机名)'''])
+	# logging.info('%d', title[u'''vpn 会话数运行时间累计用户数'''])
+	# logging.info('%d', title[u'''线路质量吞吐量和 ping累积转移日志策略'''])
+	# logging.info('%d', title[u'''ssl-vpnwindows(合适的)'''])
+	# logging.info('%d', title[u'''l2tp/ipsecwindows, mac,iphone, android无需 vpn 客户端'''])
+	# logging.info('%d', title[u'''openvpnwindows, mac,iphone, android'''])
+	# logging.info('%d', title[u'''ms-sstpwindows vista,7, 8, rt无需 vpn 客户端'''])
+	# logging.info('%d', title[u'''志愿者操作员的名字(+ 操作员的消息)'''])
+	# logging.info('%d', title[u'''总分(质量)'''])
 
 	open_vpn_list = []
 	for vpn in all_vpn_list:
-		if vpn[title[u'''OpenVPNWindows, Mac,iPhone, Android''']].a:
-			logging.debug('%s', vpn[title[u'''OpenVPNWindows, Mac,iPhone, Android''']].a['href'])
-			open_vpn_list.append(vpn[title[u'''OpenVPNWindows, Mac,iPhone, Android''']].a['href'])
+		if vpn[title[u'''openvpnwindows, mac,iphone, android''']].a:
+			logging.debug('%s', vpn[title[u'''openvpnwindows, mac,iphone, android''']].a['href'])
+			open_vpn_list.append(vpn[title[u'''openvpnwindows, mac,iphone, android''']].a['href'])
 	logging.info('all_vpn_list length %d', len(all_vpn_list))
 	logging.info('open_vpn_list length %d', len(open_vpn_list))
 
@@ -63,7 +64,8 @@ def get_open_vpn_configure_url(vpn_gate_htm_file):
 def get_open_vpn_configure_files(open_vpn_url_file):
 	html_doc = open(open_vpn_url_file, 'r')
 	soup = BeautifulSoup(html_doc)
-	href_list = [a['href'] for a in soup.findAll('a')]
+	logging.info('the html is %s \n', soup.prettify())
+	href_list = [a['href'] for a in soup.findAll('a') if a and a is not None]
 	ovpn_re = re.compile(u'''.ovpn''')
 	for hr in href_list:
 		if ovpn_re.findall(hr):
@@ -71,6 +73,13 @@ def get_open_vpn_configure_files(open_vpn_url_file):
 
 
 def main():
+	# url = 'http://112.173.79.56:45964/cn/'
+	# url_text = urllib.urlopen(url)
+	# vpn_gate_htm_file = 'VPNGate.htm'
+	# fp = open(vpn_gate_htm_file, 'w')
+	# fp.write(url_text.read())
+	# fp.close()
+
 	vpn_gate_htm_file = 'VPNGate.htm'
 	open_vpn_list = get_open_vpn_configure_url(vpn_gate_htm_file)
 	for vpn in open_vpn_list:
