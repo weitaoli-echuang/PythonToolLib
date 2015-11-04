ccc802324edd95f86e6dc5322fc6eee73402fd0e
lwt <weitaoli.echung@gmail.com>
2015-11-04 11:21:05 +0800

    generate the relative path

--- ThirdLibPathGenerate/Generate_3rd_Lib_path.py
+++ ThirdLibPathGenerate/Generate_3rd_Lib_path.py
@@ -1,11 +1,49 @@
 __author__ = 'VDTConstructor'
 
+import os
+import shutil
 
-def main():
 
+def generate_directorys(lib_name):
+	"""generate the relative path for a specical lib
+	lib_name
+		lib
+		include
+			lib_name
+		bin
+			Win32
+				Debug
+				Release
+			x64
+				Debug
+				Release"""
 
+	path_list = []
+	lib_path = os.path.abspath(lib_name)
+	rel_lib = os.path.join(lib_path, 'lib')
+	rel_include = os.path.join(lib_path, os.path.join('include', lib_name))
 
+	path_list.append(lib_path)
+	path_list.append(rel_lib)
+	path_list.append(rel_include)
+
+	platform = ['x64', 'Win32']
+	runtime = ['Debug', 'Release']
+
+	rel_bin = os.path.join(lib_path, 'bin')
+	for plat in platform:
+		for run_time in runtime:
+			tmp_path = os.path.join(plat, run_time)
+			path_list.append(os.path.join(rel_bin, tmp_path))
+
+	if os.path.isdir(lib_path):
+		shutil.rmtree(lib_path)
+
+	for path in path_list:
+		os.makedirs(path)
 
 
 if __name__ == '__main__':
-	main()
+	lib_name = 'freeglut'
+	generate_directorys(lib_name)
+
